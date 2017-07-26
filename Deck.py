bdcdaaca208c44ee0e556ad0d7c627895f783f32
balikhan <khanbali2@gmail.com>
2017-07-26 06:25:08 +0000

    Further developed deck class and added gitignore

--- Deck.py
+++ Deck.py
@@ -4,6 +4,41 @@ Created on Tue Jul 25 18:16:49 2017
 
 @author: Khochi
 """
+from Card import *
+import random
 
-class Deck():
+class Deck(object):
+    """ 
+    Deck class - holds a list of card objects
+    """
     
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1,14):
+                self.cards.append(Card(suit, rank))
+    
+    def __str__(self):
+        # constructs a string from the self.deck list
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return "\n".join(res)
+    
+    def pop_card(self):
+        # removes the last card from the list and
+        # returns it
+        return self.cards.pop()
+        
+    def add_card(self, card):
+        # adds a given card to the deck, but returns
+        # nothing.
+        self.cards.append(card)
+        
+    def shuffle(self):
+        # shuffles cards, returns nothing
+        random.shuffle(self.cards)
+        
+    def sort_deck(self):
+        # sorts cards, returns nothing
+        self.cards.sort()
--- card_unit_testing.py
+++ card_unit_testing.py
@@ -1,4 +1,5 @@
 from Card import *
+from Deck import *
 
 def check_comparisons():
     card1 = Card(1, 5)
@@ -10,7 +11,22 @@ def check_comparisons():
     assert card1 < card2
     assert card2 == card4
     assert card3 != card2
-    print("Tests successful")
+    print("Card testing successful")
     
+def check_deck():
+    main_deck = Deck()
     
-check_comparisons()
+    assert len(main_deck.cards) == 52
+    print("Shuffling main deck")
+    main_deck.shuffle()
+    #print(main_deck)
+    popped_card = main_deck.pop_card()
+    print(popped_card)
+    print("Sorting main deck")
+    main_deck.sort_deck()
+    main_deck.add_card(popped_card)
+    #print(main_deck)
+    print("Deck testing successful")
+
+#check_comparisons()
+#check_deck()
